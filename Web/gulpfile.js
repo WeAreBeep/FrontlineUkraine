035bf17927f39944e9aaab0424aae3cf3bc05cd0
Jason Ho <jasonho@oursky.com>
2021-02-06 20:52:03 +0000

    Compile Less style files by Gulp
    This aims to remove global dependency on Less compiler

---
+++ Web/gulpfile.js
@@ -0,0 +1,28 @@
+﻿// TODO: Minify and bundle CSS
+const gulp = require("gulp"),
+  less = require("gulp-less"),
+  clean = require("gulp-clean");
+
+const paths = {};
+paths.webroot = "./wwwroot/";
+paths.cssTarget = paths.webroot + "css/";
+paths.lessSrc = paths.webroot + "styles/**/*.less";
+
+gulp.task("compile:less", function () {
+  return gulp
+    .src(paths.lessSrc)
+    .pipe(less())
+    .pipe(gulp.dest(paths.cssTarget));
+});
+
+gulp.task("clean:css", function () {
+  return gulp.src(paths.cssTarget + "*.css", { read: false }).pipe(clean());
+});
+
+gulp.task("process:less", gulp.series("clean:css", "compile:less"));
+
+gulp.task("watch:less", function () {
+  gulp.watch(paths.lessSrc, gulp.series(["process:less"]));
+});
+
+gulp.task("default", gulp.series("process:less"));
