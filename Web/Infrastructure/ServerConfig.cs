5f9379c905e1197a9a9cbcc857bad4731998a02d
Jason Ho <jasonho@oursky.com>
2021-03-04 00:08:58 +0000

    Web: Set up ServerConfig configuration in DI

---
+++ Web/Infrastructure/ServerConfig.cs
@@ -0,0 +1,10 @@
+namespace Web.Infrastructure
+{
+    // TODO: Move classes in Settings.cs to ServerConfig and make it to be injected by DI system?
+    public class ServerConfig
+    {
+        public string PosttagEndpoint { get; set; }
+        public string PosttagId { get; set; }
+        public string PosttagApiKey { get; set; }
+    }
+}
