f30d36f670de4f559616f910de562b3a1fdd3ee8
Jason Ho <jasonho215@gmail.com>
2022-01-04 01:12:46 +0800

    public-web: Integrate Contentful in about page

--- src/public-web/src/containers/about/index.tsx
+++ src/public-web/src/containers/about/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useStyles } from './style';
+import { ContentfulText } from '../../components/ContentfulText';
 
 export const About: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { classes } = useStyles();
-  return <div>About page</div>;
+  return (
+    <div>
+      <ContentfulText contentType="about-us-page" />
+    </div>
+  );
 };
