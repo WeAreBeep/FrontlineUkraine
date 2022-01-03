ff00377a2baeab1a335800e941c771219ef1f345
Jason Ho <jasonho215@gmail.com>
2022-01-03 18:50:52 +0800

    public-web: Include Contentful config in AppConfig

--- src/public-web/src/config.ts
+++ src/public-web/src/config.ts
@@ -2,6 +2,12 @@ interface Config {
   mapboxToken: string;
   apiKey: string;
   apiEndpoint: string;
+  contentful: {
+    deliveryApiKey: string;
+    spaceId: string;
+    environment: string;
+    usePreviewApi: boolean;
+  };
 }
 
 export const config: Config = window.appConfig;
