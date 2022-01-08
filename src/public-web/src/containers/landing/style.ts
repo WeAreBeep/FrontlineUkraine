941a806b113eb797cb84b13b7eba05cc844a8f95
Jason Ho <jasonho215@gmail.com>
2022-01-09 04:01:54 +0800

    public-web: Fix map not expanded initially in desktop view

--- src/public-web/src/containers/landing/style.ts
+++ src/public-web/src/containers/landing/style.ts
@@ -2,6 +2,19 @@ import { createStyles } from '@mantine/styles';
 import { tablet } from '../../utils/mantine';
 
 export const useStyles = createStyles((theme) => ({
+  gridContainer: {
+    height: '100%',
+  },
+  feedContentContainer: {
+    maxHeight: '100%',
+    overflowY: 'scroll',
+  },
+  mapContainer: {
+    minHeight: 320,
+    [tablet(theme)]: {
+      minHeight: '100%',
+    },
+  },
   ourVoiceTimeline: {
     padding: `${theme.spacing.sm}px`,
     display: 'block',
