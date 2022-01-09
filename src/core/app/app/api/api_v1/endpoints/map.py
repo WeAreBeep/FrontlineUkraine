598c2afef925245d734108afa14dafa2ca96381b
Jason Ho <jasonho215@gmail.com>
2022-01-10 00:13:12 +0800

    core: Remove testing endpoint

--- src/core/app/app/api/api_v1/endpoints/map.py
+++ src/core/app/app/api/api_v1/endpoints/map.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -13,8 +11,3 @@ router = APIRouter()
 @router.get("", response_model=FeedNew)
 def get_map(db: Session = Depends(deps.get_db)) -> FeedNew:
     return views.map.get_map_data(db)
-
-
-@router.get("/test", response_model=Any)
-def get_test() -> Any:
-    return "OK"
