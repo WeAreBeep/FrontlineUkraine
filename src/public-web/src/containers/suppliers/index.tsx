90763c98753b5fdc3d812dca3ce2c8c3fe8c57fc
Louis Chan <louischan0325@gmail.com>
2022-04-19 22:06:55 +0800

    feat(public-web): Reuse the layout from / in /suppliers

--- src/public-web/src/containers/suppliers/index.tsx
+++ src/public-web/src/containers/suppliers/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useStyles } from '../landing/style';
+import { Map } from '../landing/components/Map';
+import { Col, Grid } from '@mantine/core';
+import { FeedContent } from '../landing/components/FeedContent';
+import { OurVoicesTimeline } from '../landing/components/OurVoicesTimeline';
 
 export const Suppliers: React.FC = () => {
-  return <>Hello</>;
+  const { classes } = useStyles();
+  return (
+    <Grid className={classes.gridContainer} gutter={0}>
+      <Col className={classes.feedContentContainer} span={12} md={4}>
+        <FeedContent />
+      </Col>
+      <Col className={classes.mapContainer} span={12} md={8}>
+        <Map />
+      </Col>
+      <Col span={12}>
+        <OurVoicesTimeline className={classes.ourVoiceTimeline} />
+      </Col>
+    </Grid>
+  );
 };
