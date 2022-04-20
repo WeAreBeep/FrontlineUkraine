4d64be4a9dfeae90b2420075b36be23c4106c7eb
Louis Chan <louischan0325@gmail.com>
2022-04-20 22:26:18 +0800

    feat(public-web): Add settings and sign out button

--- src/public-web/src/components/FLNavbar/index.tsx
+++ src/public-web/src/components/FLNavbar/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MantineTheme, Navbar, NavbarProps, Divider } from '@mantine/core';
 import { MainLinks } from '../MainLinks';
 import { LocaleControl } from '../LocaleControl';
+import { AccountMenu } from '../AccountMenu';
 
 const getNavbarCss = (theme: MantineTheme) => ({
   background: theme.colors['gray'][0],
@@ -13,12 +14,15 @@ export const FLNavbar: React.FC<
 > = ({ onClickLink, ...rest }) => {
   return (
     <Navbar {...rest} sx={getNavbarCss}>
-      <Navbar.Section grow={true} >
+      <Navbar.Section grow={true}>
         <MainLinks variant="navbar" onClick={onClickLink} />
       </Navbar.Section>
       <Divider my="sm" variant="solid" />
       <Navbar.Section>
         <LocaleControl fullWidth={true} />
+      </Navbar.Section>
+      <Navbar.Section>
+        <AccountMenu />
       </Navbar.Section>
     </Navbar>
   );
