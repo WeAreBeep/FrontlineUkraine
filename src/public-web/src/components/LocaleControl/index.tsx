79c50d97c2f1c7f931f5e58be9f35c92ce3dd8b9
Jason Ho <jasonho215@gmail.com>
2022-04-24 19:36:56 +0800

    fix: Fix lint error in LocaleControl

--- src/public-web/src/components/LocaleControl/index.tsx
+++ src/public-web/src/components/LocaleControl/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Button, SegmentedControlProps, Menu } from '@mantine/core';
-import { Locale } from '../../locale/type';
+import { ALL_LOCALES, Locale } from '../../locale/type';
 import { useLocale } from '../../locale/LocaleProvider';
 
 export const LocaleControl: React.FC<
@@ -9,15 +9,23 @@ export const LocaleControl: React.FC<
   const { locale, changeLocale } = useLocale();
 
   const control = (
-    <Button size="md" radius="xl" variant="white" uppercase>
+    <Button size="md" radius="xl" variant="white" uppercase={true}>
       {locale}
     </Button>
   );
 
+  const callbacks = useMemo(() => {
+    return ALL_LOCALES.reduce<Record<Locale, () => void>>((acc, key) => {
+      acc[key] = () => changeLocale(key);
+      return acc;
+      // eslint-disable-next-line @typescript-eslint/prefer-reduce-type-parameter
+    }, {} as unknown as Record<Locale, () => void>);
+  }, [changeLocale])
+
   return (
     <Menu control={control}>
-      <Menu.Item onClick={() => changeLocale(Locale.En)}>English</Menu.Item>
-      <Menu.Item onClick={() => changeLocale(Locale.Uk)}>Ukrainian</Menu.Item>
+      <Menu.Item onClick={callbacks[Locale.En]}>English</Menu.Item>
+      <Menu.Item onClick={callbacks[Locale.Uk]}>Ukrainian</Menu.Item>
     </Menu>
   );
 };
