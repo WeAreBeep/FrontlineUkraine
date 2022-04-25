79876cd163b586551475c7c8896186e69461863d
Jason Ho <jasonho215@gmail.com>
2022-04-26 02:58:15 +0800

    refactor(core): Put get_all to CRUDBase

--- src/core/app/app/crud/crud_city.py
+++ src/core/app/app/crud/crud_city.py
@@ -1,14 +1,11 @@
 from pydantic import BaseModel
-from typing import List
-from sqlalchemy.orm import Session
 
 from .base import CRUDBase
 from app.models.city import City
 
 
 class CRUDCity(CRUDBase[City, BaseModel, BaseModel]):
-    def get_all(self, db: Session,) -> List[City]:
-        return db.query(City).all()
+    pass
 
 
 city = CRUDCity(City)
