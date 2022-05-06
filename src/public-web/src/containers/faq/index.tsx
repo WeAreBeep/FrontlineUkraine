a6150aac12d5b3a3ce7533c648d3775293905086
Ten <ripplenstone@gmail.com>
2022-05-06 16:59:03 +0800

    fix syntax

--- src/public-web/src/containers/faq/index.tsx
+++ src/public-web/src/containers/faq/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useStyles } from './style';
 import { ContentfulText } from '../../components/ContentfulText';
 
-export const Faq: React.FC = () => {
+export const FAQ: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { classes } = useStyles();
   return (
     <div>
-      <ContentfulText contentType="faq"  />
+      <ContentfulText contentType="faq" />
     </div>
   );
 };
